a0c78c6770c704811d962dde70e0cb67b9f25cbb
yntonline <yntonline25@gmail.com>
2025-07-30 07:11:13 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hello, lucky"
+  return  render_template('home.html')
 
 
 if __name__== "__main__":
